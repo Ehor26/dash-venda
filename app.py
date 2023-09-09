dcb66bf9d3d9df084d1fa18ef64e4508e4d2a1d9
Estevam H. O. Rizek <32528638+Ehor26@users.noreply.github.com>
2023-09-09 15:12:56 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ import dash
 import dash_bootstrap_components as dbc
 
 
-app = dash.Dash(__name__, suppress_callback_exceptions=True)
-
+#app = dash.Dash(__name__, suppress_callback_exceptions=True)
+ app = dash.Dash(__name__)
 app.scripts.config.serve_locally = True
 server = app.server
- 
+

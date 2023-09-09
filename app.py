40e580aefe7da304553546c1e67320a47dffe688
Estevam H. O. Rizek <32528638+Ehor26@users.noreply.github.com>
2023-09-09 15:51:07 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,7 @@ import dash
 import dash_bootstrap_components as dbc
 
 
-app = dash.Dash(__name__, suppress_callback_exceptions=True)
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP], suppress_callback_exceptions=True)
 
 #app.scripts.config.serve_locally = True
 server = app.server
-
